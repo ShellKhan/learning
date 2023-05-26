98ccceab9549fdba16cc46d03cfdcad1233b65eb
Jorian <jorian@mail.ru>
2023-05-26 15:08:57 +0300

    26.05 2

--- 0526/js/3.js
+++ 0526/js/3.js
@@ -1,3 +1,40 @@
 $(function() {
-    
+    let deltaX, deltaY;
+    /*
+    $('.pink').on({
+        mousedown: function(e) {
+            $(this).addClass('inuse');
+            deltaX = e.clientX - $(this).position().left;
+            deltaY = e.clientY - $(this).position().top;
+        },
+        mousemove: function(e) {
+            if ($(this).hasClass('inuse')) {
+                $(this).css({
+                    left: (e.clientX - deltaX) + 'px',
+                    top: (e.clientY - deltaY) + 'px'
+                });
+            }
+        },
+        mouseup: function() {
+            $(this).removeClass('inuse');
+        },
+        mouseout: function() {
+            $(this).removeClass('inuse');
+        }
+    });
+    */
+    $('.pink').on('mousedown', function(e) {
+        $(this).addClass('inuse');
+        deltaX = e.clientX - $(this).position().left;
+        deltaY = e.clientY - $(this).position().top;
+    }).on('mousemove', function(e) {
+        if ($(this).hasClass('inuse')) {
+            $(this).css({
+                left: (e.clientX - deltaX) + 'px',
+                top: (e.clientY - deltaY) + 'px'
+            });
+        }
+    }).on('mouseup mouseout', function() {
+        $(this).removeClass('inuse');
+    });
 });
