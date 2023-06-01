2d37c93d61652492dc674e7a328ab20b1c15577b
Jorian <jorian@mail.ru>
2023-06-01 15:38:59 +0300

    01.06

--- project/js/main.js
+++ project/js/main.js
@@ -4,6 +4,31 @@ let actionend;
 
 
 /* main */
+$(function() {
+    if ($('.actiontimer').length) {
+        actionTimer(actionend);
+        setTimeout(function ac_tim() {
+            actionTimer(actionend);
+            setTimeout(ac_tim, 950);
+        }, 950);
+    }
+    
+    $('.slider').each(function() {
+        makeSlider($(this));
+    });
+    
+    /*
+    if ($('.slider').length) {
+        setTimeout(function sld() {
+            document.querySelector('.next').addEventListener('transitionend', function() {
+                setTimeout(sld, 1000);
+            }, {once: true});
+            slider();
+        }, 1000);
+    }*/
+    
+});
+/*
 document.addEventListener('DOMContentLoaded', function() {
     if (document.querySelector('.actiontimer')) { // проверяем, что на текущей странице есть нужный элемент. поскольку файл скриптов общий, на каких-то страницах может не быть нужного (в данном случае - таймера акции).
         actionTimer(actionend);
@@ -12,35 +37,43 @@ document.addEventListener('DOMContentLoaded', function() {
             setTimeout(ac_tim, 950);
         }, 950);
     }
+    
     if (document.querySelector('.sliderwindow')) {
         setTimeout(function sld() {
+            document.querySelector('.next').addEventListener('transitionend', function() {
+                setTimeout(sld, 1000);
+            }, {once: true});
             sliderGo();
-            setTimeout(sld, 5000);
-        }, 5000);
+        }, 1000);
     }
     
-    
-    
-    
-    
 });
+*/
 
 
 
 /* action timer */
 function actionTimer(end) {
     let delta = Math.floor((Date.parse(end) - Date.now()) / 1000);
-    let seconds = delta % 60;
-    delta = Math.floor(delta / 60);
-    let minutes = delta % 60;
-    delta = Math.floor(delta / 60);
-    let hours = delta % 24;
-    delta = Math.floor(delta / 24);
+    if (delta > 0) {
+        let seconds = delta % 60;
+        delta = Math.floor(delta / 60);
+        let minutes = delta % 60;
+        delta = Math.floor(delta / 60);
+        let hours = delta % 24;
+        delta = Math.floor(delta / 24);
+        $('.actiontimer').html("<b>" + delta + "</b>" + grammatics(delta, 'день', 'дня', 'дней') +"<b>" + hours + "</b>" + grammatics(hours, 'час', 'часа', 'часов') + "<b>" + addZero(minutes) + "</b>" +grammatics(minutes, 'минута', 'минуты', 'минут') + "<b>" + addZero(seconds) + "</b><span>" + grammatics(seconds, 'секунда', 'секунды', 'секунд') + "</span>");
+    } else {
+        $('.actiontimer').html("<b>0</b>дней<b>0</b>часов<b>00</b>минут<b>00</b><span>секунд</span>");
+        //$('.action').remove();
+    }
+    /*
     // document.querySelector('.actiontimer').innerHTML = `<b>${delta}</b>${grammatics(delta, 'день', 'дня', 'дней')}<b>${hours}</b>${grammatics(hours, 'час', 'часа', 'часов')}<b>${addZero(minutes)}</b>${grammatics(minutes, 'минута', 'минуты', 'минут')}<b>${addZero(seconds)}</b><span>${grammatics(seconds, 'секунда', 'секунды', 'секунд')}</span>`;
     document.querySelector('.actiontimer').innerHTML = "<b>" + delta + "</b>" + grammatics(delta, 'день', 'дня', 'дней') +"<b>" + hours + "</b>" + grammatics(hours, 'час', 'часа', 'часов') + "<b>" + addZero(minutes) + "</b>" +grammatics(minutes, 'минута', 'минуты', 'минут') + "<b>" + addZero(seconds) + "</b><span>" + grammatics(seconds, 'секунда', 'секунды', 'секунд') + "</span>";
+    */
 }
 
-/* slider */
+/* slider - вынесено в отдельный файл
 function sliderGo() {
     let slides = Array.from(document.querySelectorAll('.sliderwindow .slide'));
     let idx = slides.indexOf(document.querySelector('.sliderwindow .next'));
@@ -53,6 +86,7 @@ function sliderGo() {
     document.querySelector('.sliderwindow .next').classList.remove('next');
     slides[idx].classList.add('next');
 }
+*/
 
 /* utilites */
 function addZero(num) {
